60f86522cebb94c529a7803188864b472b9b3447
novikovkiruha <novikov.k.ru@gmail.com>
2019-03-30 12:17:58 +0200

    release

--- CSharp_Course_Project/Program.cs
+++ CSharp_Course_Project/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharp_Course_Project
 {
@@ -10,6 +6,7 @@ namespace CSharp_Course_Project
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("watta fuck???");
         }
     }
 }
