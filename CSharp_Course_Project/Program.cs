c34aec23538e4089738416123cade5f39198f558
novikovkiruha <novikov.k.ru@gmail.com>
2019-03-30 15:26:21 +0200

    volume, area, average

--- CSharp_Course_Project/Program.cs
+++ CSharp_Course_Project/Program.cs
@@ -6,19 +6,46 @@ namespace CSharp_Course_Project
     {
         static void Main(string[] args)
         {
-            int i1 = 435;
-            int i2 = 24500;
-            decimal dec = 7.98845m;
-
-            Console.WriteLine((short)i1);
-            Console.WriteLine((short)i2);
-
-            Console.WriteLine((int)dec);
+            Average();
+            Volume();
+            Area();
         }
 
         static int Sum(int x, int y)
         {
             return x + y;
+        }
+
+        public static void Average()
+        {
+            Console.WriteLine("Enter 3 numbers:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            double average = (double)(a + b + c) / 3;
+            Console.WriteLine(average);
+        }
+
+        public static void Volume()
+        {
+            const double PI = Math.PI;
+            Console.Write("Enter a radius: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter a height: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            double volume = PI * radius * 2 * height;
+            Console.WriteLine(volume);
+        }
+
+        public static void Area()
+        {
+            const double PI = Math.PI;
+            Console.Write("Enter a radius: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter a height: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            double area = 2 * PI * radius * (radius + height);
+            Console.WriteLine(area);
         }
     }
 }
