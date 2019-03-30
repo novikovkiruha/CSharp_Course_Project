a6d37ffe4c654776261dce97632487795f05f08a
novikovkiruha <novikov.k.ru@gmail.com>
2019-03-30 12:59:32 +0200

    added square circle

--- CSharp_Course_Project/Program.cs
+++ CSharp_Course_Project/Program.cs
@@ -6,7 +6,25 @@ namespace CSharp_Course_Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Watta fuck??? Doesn't work");
+            squareCircle();
+            //Console.WriteLine(squareCircle2(4)); 
+        }
+
+        public static void squareCircle()
+        {
+            const double pi = Math.PI;
+            Console.Write("Enter a radius of a circle: ");
+            string radius = Console.ReadLine();
+            double r = Convert.ToInt32(radius);
+            double squareCircle = (r*r) * pi;
+            Console.WriteLine(squareCircle);
+        }
+
+        public static double squareCircle2(int r)
+        {
+            const double pi = Math.PI;
+            double square = (r * r) * pi;
+            return square;
         }
     }
 }
