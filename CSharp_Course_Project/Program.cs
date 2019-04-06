03a015afbb36ba9b095bd86eed890dbfd9b6853d
novikovkiruha <novikov.k.ru@gmail.com>
2019-04-06 11:22:30 +0300

    second task

--- CSharp_Course_Project/Program.cs
+++ CSharp_Course_Project/Program.cs
@@ -6,14 +6,31 @@ namespace CSharp_Course_Project
     {
         static void Main(string[] args)
         {
-            SecondsAndHours();
+            //SecondsAndHours();
+            ThreeNumbers();
         }
 
         public static void SecondsAndHours()
         {
             int seconds = Convert.ToInt32(Console.ReadLine());
-            double hours = (double)seconds / 3600;
+            int hours = seconds / 3600;
             Console.WriteLine(hours);
+        }
+
+        public static void ThreeNumbers()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if ((b > a && b < c) || (b > c && b < a))
+            {
+                Console.WriteLine("B is between A and C");
+            }
+            else
+            {
+                Console.WriteLine("B is not betweeen A and C");
+            }
         }
     }
 }
