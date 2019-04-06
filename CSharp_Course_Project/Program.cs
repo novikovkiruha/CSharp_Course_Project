9f2968de140054775acd0347faf1ee7b02ac9f2a
novikovkiruha <novikov.k.ru@gmail.com>
2019-04-06 11:09:39 +0300

    first task

--- CSharp_Course_Project/Program.cs
+++ CSharp_Course_Project/Program.cs
@@ -6,7 +6,14 @@ namespace CSharp_Course_Project
     {
         static void Main(string[] args)
         {
+            SecondsAndHours();
+        }
 
+        public static void SecondsAndHours()
+        {
+            int seconds = Convert.ToInt32(Console.ReadLine());
+            double hours = (double)seconds / 3600;
+            Console.WriteLine(hours);
         }
     }
 }
