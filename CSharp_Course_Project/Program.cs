e542da1f34ec6b30df0376840c26226e9009435b
novikovkiruha <novikov.k.ru@gmail.com>
2019-04-06 11:56:18 +0300

    fifth task

--- CSharp_Course_Project/Program.cs
+++ CSharp_Course_Project/Program.cs
@@ -9,7 +9,8 @@ namespace CSharp_Course_Project
             //SecondsAndHours();
             //ThreeNumbers();
             //OddAndThreeDigitNumber();
-            ThreeNumbersOperations();
+            //ThreeNumbersOperations();
+            ConsoleCalculator();
         }
 
         public static void SecondsAndHours()
@@ -54,15 +55,52 @@ namespace CSharp_Course_Project
         {
             int x = 10, y = 12, z = 3;
             x += y - x++ * z; // 10 + 12 - (10 * 3)
-            Console.WriteLine(x);
-            z = --x - y * 5; // 10 + 12 - (11 * 3)
+            Console.WriteLine(x); //-8
+            z = --x - y * 5; // -9 - (12 * 5)
             Console.WriteLine(z);
-            y /= x + 5 % z; // 10 + 12 - (11 * 3)
+            y /= x + 5 % z;
             Console.WriteLine(y);
-            z = x++ + y * 5; // 10 + 12 - (11 * 3)
+            z = x++ + y * 5;
             Console.WriteLine(z);
-            x = y - x++ * z; // 10 + 12 - (11 * 3)
+            x = y - x++ * z;
             Console.WriteLine(x);
+        }
+
+        public static void ConsoleCalculator()
+        {
+            Console.Write("Enter the first number: ");
+            int operand1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            int operand2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the operation symbol: ");
+            string symbol = Console.ReadLine();
+
+            int sum = operand1 + operand2;
+            int subtraction = operand1 - operand2;
+            int multiply = operand1 * operand2;
+            int division = operand1 / operand2;
+
+            switch (symbol)
+            {
+                case "+":
+                    Console.WriteLine(sum);
+                    break;
+                case "-":
+                    Console.WriteLine(subtraction);
+                    break;
+                case "*":
+                    Console.WriteLine(multiply);
+                    break;
+                case "/":
+                    Console.WriteLine(division);
+                    if (operand2 == 0)
+                    {
+                        Console.WriteLine("Cann't divide by 0");
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
