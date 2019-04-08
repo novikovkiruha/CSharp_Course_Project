0e059375949c8be473de8ced066520bead3b9413
novikovkiruha <novikov.k.ru@gmail.com>
2019-04-08 21:33:32 +0300

    Added NumberProcesses() method (3rd task)

--- CSharp_Course_Project/Program.cs
+++ CSharp_Course_Project/Program.cs
@@ -6,31 +6,47 @@ namespace CSharp_Course_Project
     {
         public static void NumberProcesses()
         {
-            string n = Console.ReadLine();
-            int number = Convert.ToInt32(n);
-
-            if (number == 0)
-                Console.WriteLine($"Number is 0");
-            else if (number < 0)
-                Console.WriteLine("Negative number");
-            else if (number > 0)
-                Console.WriteLine("Positive number");
-
-            //int primeNumber = number;
-            //for (int i = 1; i <= number; i++)
-            //{
-            //    if (i / number == 1 && number / 1 == number)
-            //        primeNumber = number;
-            //}
-            //Console.WriteLine($"{number} is a prime number");
-
-            int[] array = new int[5] { 9, 5, 3, 6, 9 }; // 0 = 2, 1 = 5, 2 = 3
-            for (int i = 0; i < array.Length; i++)
+            while (true)
             {
-                if (number % array[i] == 0)
+                Console.Write("Enter the number: ");
+                string stringNumber = Console.ReadLine();
+                bool isNumber = Int32.TryParse(stringNumber, out int number);
+
+                if (!isNumber)
                 {
-                    Console.WriteLine($"The number is divided by {array[i]}");
+                    Console.WriteLine("Not a numeric value. Try again...");
+                    continue;
                 }
+
+                if (number == 0)
+                    Console.WriteLine($"Number is 0");
+                else if (number < 0)
+                    Console.WriteLine("Negative number");
+                else if (number > 0)
+                    Console.WriteLine("Positive number");
+
+                int count = 0;
+                for (int i = 1; i <= number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                    Console.WriteLine($"{number} is a prime number");
+                else
+                    Console.WriteLine($"{number} is NOT a prime number");
+
+                int[] array = new int[5] { 9, 5, 3, 6, 9 }; // 0 = 2, 1 = 5, 2 = 3
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (number % array[i] == 0)
+                    {
+                        Console.WriteLine($"The number is divided by {array[i]}");
+                    }
+                }
+                continue;
             }
         }
 
