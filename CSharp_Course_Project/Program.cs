359d8ea34e2b4384b32825dce4ac783a2ed8f56d
novikovkiruha <novikov.k.ru@gmail.com>
2019-04-08 22:14:09 +0300

    Added Logistics() method

--- CSharp_Course_Project/Program.cs
+++ CSharp_Course_Project/Program.cs
@@ -50,9 +50,55 @@ namespace CSharp_Course_Project
             }
         }
 
+        //Имеется N клиентов, которым компания производитель должна доставить товар.
+        //Сколько существует возможных маршрутов доставки товара, с учетом того, что товар будет доставлять одна машина?  
+        //Напишите программу, которая будет рассчитывать, и выводить на экран количество возможных вариантов доставки товара.
+        //Для решения задачи, используйте факториал N!, рассчитываемый с помощью цикла do-while.
+        public static void Logistics()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of clients: ");
+                string stringNumber = Console.ReadLine();
+                bool isNumber = Int32.TryParse(stringNumber, out int clientsNumber);
+                int routes = clientsNumber;
+                if (!isNumber)
+                {
+                    Console.WriteLine("Not a numeric value. Try again...");
+                    continue;
+                }
+                else if (clientsNumber == 0)
+                {
+                    Console.WriteLine("Number of clients cann't be 0. Try again...");
+                    continue;
+                }
+                else if (clientsNumber == 1)
+                {
+                    Console.WriteLine($"Number of routes: {routes}");
+                }
+                else
+                {
+                    do
+                    {
+                        routes *= clientsNumber - 1;
+                        clientsNumber--;
+                    }
+                    while (clientsNumber != 1);
+                    Console.WriteLine($"Number of routes: {routes}");
+                }
+                Console.WriteLine("Do you want to proceed? Y/N");
+                string proceed = Console.ReadLine();
+                if (proceed.Equals("Y") || proceed.Equals("y"))
+                    continue;
+                else
+                    break;
+            }
+        }
+
         static void Main(string[] args)
         {
-            NumberProcesses();
+            //NumberProcesses();
+            Logistics();
         }
     }
 }
