8110537adfbe8cf7b2017ff6d590360671b145d5
novikovkiruha <novikov.k.ru@gmail.com>
2019-04-11 23:17:12 +0300

    Added MaxDigit() method

--- CSharp_Course_Project/Program.cs
+++ CSharp_Course_Project/Program.cs
@@ -4,9 +4,37 @@ namespace CSharp_Course_Project
 {
     class Program
     {
+        public static void MaxDigit()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number: ");
+                string stringNumber = Console.ReadLine();
+                bool isNumber = Int32.TryParse(stringNumber, out int number);
+                if (!isNumber || number < 0)
+                {
+                    Console.WriteLine("Not a numeric or natural value. Try again...");
+                    continue;
+                }
+                int max = 0;
+                for (int i = 0; i < stringNumber.Length; i++)
+                {
+                    int temp = number % 10;
+                    if (temp > max) max = temp;
+                    number /= 10;
+                }
+                Console.WriteLine(max);
+
+                Console.WriteLine("Would you like to continue? Y/N");
+                string proceed = Console.ReadLine();
+                if (proceed.ToLower() == "n") break;
+                else continue;
+            }
+        }
+
         static void Main(string[] args)
         {
-
+            MaxDigit();
         }
     }
 }
