22d68041536be0acdcc4859779fa3bd637e4786c
novikovkiruha <novikov.k.ru@gmail.com>
2019-04-13 13:56:24 +0300

    Added MyReverse() method (task 5)

--- CSharp_Course_Project/Program.cs
+++ CSharp_Course_Project/Program.cs
@@ -4,11 +4,18 @@ namespace CSharp_Course_Project
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int[] getArray()
         {
-            //ArrayValues();
-            //ArrayDuplicates();
-            ArrayIndex();
+            Console.Write("Enter the sire of the array: ");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[arraySize];
+            var randomNubmers = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = randomNubmers.Next(0, 10);
+            }
+
+            return array;
         }
 
         public static void ArrayValues()
@@ -43,27 +50,33 @@ namespace CSharp_Course_Project
             Console.WriteLine($"Min = {min}");
         }
 
-        public static void ArrayDuplicates()
+        // note: сделать с запоминанием индекса и пропускать индекс, если 
+        public static void ArrayDuplicates(int[] array)
         {
-            int[] array = new int[] { 5, 4, 9, 2, 5, 6, 0, 2, 1, 0 };
+            int[] arrayWtDuplicates = new int[array.Length];
+            arrayWtDuplicates[0] = array[0];
 
             int count = 0;
             for (int i = 0; i < array.Length; i++)
             {
-                for (int j = i + 1; j < array.Length; j++)
+                for (int j = i + 1; j < arrayWtDuplicates.Length; j++)
                 {
-                    if (array[i] == array[j])
+                    if (array[i] != arrayWtDuplicates[j])
+                        arrayWtDuplicates[j] = array[i];
+                    else
                     {
                         Console.WriteLine($"Duplicate: {array[i]}");
-                        count++;
+                        continue;
                     }
                 }
             }
 
-            int[] array2 = new int[array.Length - count];
-            Console.WriteLine(array2.Length);
-
             foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            foreach (var item in arrayWtDuplicates)
             {
                 Console.WriteLine(item);
             }
@@ -82,13 +95,73 @@ namespace CSharp_Course_Project
             }
             Console.WriteLine(string.Join(",", array));
 
-            int min = -50;
-            int max = 50;
+            int min = -5;
+            int max = 5;
             for (int i = 0; i < array.Length; i++)
             {
                 if (array[i] > min && array[i] < max)
                     Console.WriteLine($"Index of {array[i]} = {i}");
             }
+        }
+
+        //Дан одномерный массив. Найти среднее арифметическое его элементов. 
+        //Вывести на экран только те элементы массива, которые больше найденного среднего арифметического.
+        public static void ArrayAverage()
+        {
+            Console.Write("Enter the sire of the array: ");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[arraySize];
+            var randomNubmers = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = randomNubmers.Next(-100, 100);
+            }
+            Console.WriteLine(string.Join(",", array));
+
+            int average = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                average += array[i];
+            }
+            average /= array.Length;
+            Console.WriteLine(average);
+        }
+        //Создать метод MyReverse(int [] array), 
+        //который принимает в качестве аргумента массив целочисленных элементов и возвращает инвертированный массив 
+        //(элементы массива в обратном порядке).
+        public static void MyReverse(int[] array)
+        {
+            Console.WriteLine("First array");
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+            int getLastElement = array.Length;
+            int[] invertedArray = new int[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int temp = array[i];
+                array[i] = invertedArray[getLastElement - 1 - i];
+                invertedArray[getLastElement - 1 - i] = temp;
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Inverted array");
+            foreach (var item in invertedArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //ArrayValues();
+            //ArrayDuplicates(getArray());
+            //ArrayIndex();
+            //ArrayAverage();
+            MyReverse(getArray());
         }
     }
 }
