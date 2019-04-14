46ec056cfcb99be200d23be51ff1f8d11e2990b2
novikovkiruha <novikov.k.ru@gmail.com>
2019-04-14 22:53:02 +0300

    Fixed ArrayValues() method (task 1)

--- CSharp_Course_Project/Program.cs
+++ CSharp_Course_Project/Program.cs
@@ -20,36 +20,29 @@ namespace CSharp_Course_Project
             return array;
         }
 
-        public static void ArrayValues() // 1
+        public static void ArrayValues(int[] array) // 1
         {
-            Console.Write("Enter the size of the array: ");
-            int arraySize = Convert.ToInt32(Console.ReadLine());
-            int[] array = new int[arraySize];
-            var randomNubmers = new Random();
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = randomNubmers.Next(-100, 100);
-            }
-
             int max = array[0];
             int min = array[0];
             int sum = 0;
             int average = array[0];
-            int odd = array[0];
-            int even = array[0];
+            string odd = "Odd: ";
+            string even = "Even: ";
             for (int i = 0; i < array.Length; i++)
             {
                 sum += array[i];
                 average = sum / array.Length;
                 if (array[i] > max) max = array[i];
                 if (array[i] < min) min = array[i];
-                if (array[i] % 2 == 0) Console.WriteLine($"{array[i]} is even");
-                if (array[i] % 2 != 0) Console.WriteLine($"{array[i]} is odd");
+                if (array[i] % 2 == 0) even += $"{array[i]}, ";
+                if (array[i] % 2 != 0) odd += $"{array[i]}, ";
             }
             Console.WriteLine($"Sum = {sum}");
             Console.WriteLine($"Avg = {average}");
             Console.WriteLine($"Max = {max}");
             Console.WriteLine($"Min = {min}");
+            Console.WriteLine(odd);
+            Console.WriteLine(even);
         }
 
         public static void ArrayDuplicates(int[] array) // 2
@@ -213,10 +206,10 @@ namespace CSharp_Course_Project
 
         static void Main(string[] args)
         {
-            //ArrayValues();
+            ArrayValues(getArray());
             ArrayDuplicates(getArray());
-            //ArrayIndex();
-            //ArrayAverage();
+            //ArrayIndex(getArray());
+            //ArrayAverage(getArray());
             //MyReverse(getArray());
             //SubArray(getArray(), 1, 3);
         }
