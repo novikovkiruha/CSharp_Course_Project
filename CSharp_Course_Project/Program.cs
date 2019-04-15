56b941db2ab07b102183622454c425f3917c3660
novikovkiruha <novikov.k.ru@gmail.com>
2019-04-15 23:17:29 +0300

    Added BinaryArray() method (task 1)

--- CSharp_Course_Project/Program.cs
+++ CSharp_Course_Project/Program.cs
@@ -4,9 +4,47 @@ namespace CSharp_Course_Project
 {
     class Program
     {
+        public static void BinaryArray() // 1
+        {
+            Console.Write("Enter the size of the array: ");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[arraySize];
+
+            Console.Write("Enter array values: ");
+            for (int i = 0; i < array.Length; i++)
+                array[i] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(string.Join(",", array));
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[0] != 0)
+                {
+                    for (int j = 0; j < array[i]; j++)
+                    {
+                        if (i % 2 == 0)
+                            Console.Write("0");
+                        else
+                            Console.Write("1");
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < array[i]; j++)
+                    {
+                        if (i % 2 == 0)
+                            Console.Write("0");
+                        else
+                            Console.Write("1");
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
-
+            BinaryArray();
         }
     }
 }
