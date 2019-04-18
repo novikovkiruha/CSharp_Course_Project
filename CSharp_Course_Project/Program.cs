f02acc856786f15888ebdcdf58d8e969cf342f08
novikovkiruha <novikov.k.ru@gmail.com>
2019-04-18 23:14:11 +0300

    Added - SpacesBetweenQuestions() method (task 4) - SpacesDeleting() method (task 5) - FirstWordSymbol() method (task 6)

--- CSharp_Course_Project/Program.cs
+++ CSharp_Course_Project/Program.cs
@@ -26,7 +26,7 @@ namespace CSharp_Course_Project
         }
 
         //Вставить в заданную позицию строки другую строку.
-        public static void StringInput() // 2
+        public static void StringInput() // 3
         {
             Console.WriteLine("Enter the first text:");
             string firstText = Console.ReadLine();
@@ -39,13 +39,82 @@ namespace CSharp_Course_Project
         }
 
         //Во введенной строке удалить пробелы между первым и вторым вопросительным знаком.
+        public static void SpacesBetweenQuestions() // 4
+        {
+            string text = "Why're you seeing so serious? Ha? WHAT'RE YOU LOOKING FOR?";
+            int questionIndex = 0;
+
+            string newText = "";
+            int counter = 0;
+            while (text[counter] != '?')
+            {
+                newText += text[counter];
+                counter++;
+                questionIndex = counter;
+            }
+
+            for (int i = questionIndex; i < text.Length; i++)
+            {
+                if (text[i] != ' ')
+                {
+                    newText += text[i];
+                }
+            }
+            text = newText;
+            Console.WriteLine(text);
+        }
 
         //Удалить в строке все лишние пробелы.
         //Лишними считаются пробелы, следующие непосредственно за пробелами. Т.е.между словами всегда должен находиться один пробел.
+        public static void SpacesDeleting() // 5
+        {
+            string text = " some   text     is  here  ";
+
+            //
+            // 1st option
+            //
+            string[] textWithoutExtraSpaces = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine($"|{string.Join(" ", textWithoutExtraSpaces)}|");
+
+            //
+            // 2nd option
+            //
+            string newText = "";
+            string[] array = text.Split(' ');
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != "")
+                {
+                    newText += array[i] + ' ';
+                }
+            }
+            text = newText;
+            Console.WriteLine($"|{text.Trim()}|");
+        }
 
         //Найти слово, стоящее в тексте под определенным номером, и вывести его первую букву.
+        public static void FirstWordSymbol() // 6
+        {
+            string text = "Why're you seeing so serious? Ha?";
+            string[] array = text.Split(' ');
+            Console.Write("Enter the index of the first string: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+            if (index > array.Length)
+            {
+                Console.WriteLine("Index is outside the array. Try again...");
+            }
 
-        //Вводится строка слов.Вывести слова в обратном порядке.
+            Console.WriteLine(text);
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == index)
+                {
+                    Console.WriteLine(array[i].Substring(0, 1));
+                }
+            }
+        }
+
+        //Вводится строка слов. Вывести слова в обратном порядке.
 
         //В строке, состоящей из слов, разделенных пробелами, определить длину кратчайшего и самого длинного слов.
 
@@ -53,7 +122,10 @@ namespace CSharp_Course_Project
         static void Main(string[] args)
         {
             //ExactSymbol();
-            StringInput();
+            //StringInput();
+            //SpacesBetweenQuestions();
+            SpacesDeleting();
+            //FirstWordSymbol();
         }
     }
 }
