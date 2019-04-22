eef8841c8ef06dae1403a96ed1894449a2ff7292
novikovkiruha <novikov.k.ru@gmail.com>
2019-04-22 23:02:56 +0300

    Whole HomeTask4_strings was added

---
+++ CSharp_Course_Project/Task1_JadenSmithCase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSharp_Course_Project
+{
+    class Task1_JadenSmithCase
+    {
+        public static void JadenSmithCase()
+        {
+            var jadenCasedText = FirstLetterToUpperCase();
+            Console.WriteLine(string.Join(" ", jadenCasedText));
+        }
+
+        public static string[] GetInputTextArray()
+        {
+            Console.WriteLine("Enter some text:");
+            string text = lib.GetText();
+            string[] textArray = lib.ExtraSpacesDeleting(text);
+
+            return textArray;
+        }
+
+        public static string[] FirstLetterToUpperCase()
+        {
+            var textArray = GetInputTextArray();
+            string[] upperCaseArray = new string[textArray.Length];
+            for (int i = 0; i < upperCaseArray.Length; i++)
+            {
+                upperCaseArray[i] = textArray[i].Substring(0, 1).ToUpper() + textArray[i].Substring(1, textArray[i].Length - 1);
+            }
+
+            return upperCaseArray;
+        }
+    }
+}
---
+++ CSharp_Course_Project/lib.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Course_Project
+{
+    class lib
+    {
+        public static string GetText()
+        {
+            Console.WriteLine("Enter some text:");
+            string text = Console.ReadLine();
+
+            return text;
+        }
+
+        public static string[] ExtraSpacesDeleting(string text)
+        {
+            string[] textWithoutExtraSpaces = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            return textWithoutExtraSpaces;
+        }
+    }
+}
