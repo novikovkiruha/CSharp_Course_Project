bb6fcebfb986a1c60e5662bac69ce58c372f3811
novikovkiruha <novikov.k.ru@gmail.com>
2019-04-27 14:19:29 +0300

    Added Task2 class

--- CSharp_Course_Project/Task2/MyStruct.cs
+++ CSharp_Course_Project/Task2/MyStruct.cs
@@ -6,8 +6,18 @@ using System.Threading.Tasks;
 
 namespace CSharp_Course_Project.Task2
 {
-    struct MyStruct
+    public struct MyStruct
     {
+        private string change;
 
+        public string Execute()
+        {
+            return this.change;
+        }
+
+        public void SetStructureFields(string fieldValue)
+        {
+            this.change = fieldValue;
+        }
     }
 }
