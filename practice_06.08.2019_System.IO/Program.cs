aadd77b1d5fcca21796f3789c155dbc93fb2b062
novikovkiruha <novikov.k.ru@gmail.com>
2019-06-08 13:46:59 +0300

    Added Task1_Folder

---
+++ practice_06.08.2019_System.IO/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using practice_06._08._2019_System.IO.Task1_Folders;
+
+namespace practice_06._08._2019_System.IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Folder().ShowFolders();
+        }
+    }
+}
---
+++ practice_06.08.2019_System.IO/Task1_Folders/Folder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace practice_06._08._2019_System.IO.Task1_Folders
+{
+    public class Folder
+    {
+        public void ShowFolders()
+        {
+            var path = @"D:\";
+            //var path = @"C:\Windows\System32\";
+            var folders = Directory.GetDirectories(path);
+            var folderNumber = 0;
+            long totalSize = 0;
+
+            foreach (var item in folders)
+            {
+                try
+                {
+                    DirectoryInfo folder = new DirectoryInfo(item);
+                    Console.WriteLine($"Folder Name: {folder.Name}");
+                    folderNumber++;
+                    foreach (var file in folder.GetFiles())
+                    {
+                        totalSize += file.Length;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Couldn't define the size of the folder");
+                }
+            }
+
+            Console.WriteLine($"Number of folders: {folderNumber}");
+            Console.WriteLine($"Total size: {totalSize}");
+        }
+    }
+}
