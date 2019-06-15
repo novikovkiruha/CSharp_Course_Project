6b17651cd4a1e176475202a0aa2e4ad75cf06c8d
novikovkiruha <novikov.k.ru@gmail.com>
2019-06-15 11:09:56 +0300

    Task1

---
+++ practice_06.15.2019_generics/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_06._15._2019_generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
