537ac1da4ca92b2c5c87776007d8d620eb8934b0
novikov.k.ru@gmail.com <novikov.k.ru@gmail.com>
2019-06-18 20:09:02 +0300

    development changes

--- CSharp_Course_Project/Collections.cs
+++ CSharp_Course_Project/Collections.cs
@@ -23,6 +23,36 @@ namespace CSharp_Course_Project
             {
                 Console.WriteLine(item);
             }
+
+            //read only collection
+            IReadOnlyCollection<int> roc = new ReadOnlyCollection<int>(new Collection<int>());
+
+            List<int> list = new List<int>(); // не поддерживает кастомизацию
+            Collection<int> collection = new Collection<int>(); // поддерживает кастомизацию: содержит некоторые virtual/override методы
+            ObservableCollection<int> obCol = new ObservableCollection<int>(); //выдача уведомлений при получении и удалении элементов: CollectionChanged
+            LinkedList<int> linked = new LinkedList<int>(); // двунаправленный список
+            Stack<int> stack = new Stack<int>(); // последний пришел - первый вышел (LIFO)
+            Queue<int> queue = new Queue<int>(); // первый пришел - первый вышел
+            SortedList<int, string> sortedList = new SortedList<int, string>(); // сортирует по ключу. Реализован в виде списка
+            SortedDictionary<int, string> sortedDictionary = new SortedDictionary<int, string>(); // дерево бинарного поиска, где все объекты отсортированы по ключу. Бинарное дерево поиска. Реализован в виде словаря
+
+            Dictionary<int, string> dictionary = new Dictionary<int, string>(); // отношение ключ-значение или hash-таблица
+            dictionary.Add(1, "1");
+            dictionary.Add(2, "2");
+            dictionary.Add(3, "3");
+            dictionary.Add(4, "4");
+            dictionary.Add(5, "5");
+            //dictionary.Add(5, "6");
+
+            foreach (KeyValuePair<int, string> item in dictionary)
+            {
+                Console.WriteLine(item.GetHashCode() + " - " + item.Key + "-" + item.Value);
+            }
+            Console.WriteLine(dictionary.Comparer);
+
+            string nextKey;
+
+            
         }
     }
 }
