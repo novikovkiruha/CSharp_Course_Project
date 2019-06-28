ffbcdf2be16592d5061bfe4baddf79c695823385
novikovkiruha <novikov.k.ru@gmail.com>
2019-06-28 11:44:11 +0300

    Task1_Calculator was added

---
+++ practice_06.28.2019_delegates/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using practice_06._28._2019_delegates.Task1_Calculator;
+
+namespace practice_06._28._2019_delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Execution().Execute();
+        }
+    }
+}
---
+++ practice_06.28.2019_delegates/Task1_Calculator/Calculator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_06._28._2019_delegates.Task1_Calculator
+{
+    public delegate double Calculate(double firstArgument, double secondArgument);
+
+    public class Calculator
+    {
+        public double Add(double firstArgument, double secondArgument)
+        {
+            return firstArgument + secondArgument;
+        }
+
+        public double Subtract(double firstArgument, double secondArgument)
+        {
+            return firstArgument - secondArgument;
+        }
+
+        public double Multiply(double firstArgument, double secondArgument)
+        {
+            return firstArgument * secondArgument;
+        }
+
+        public double Divide(double firstArgument, double secondArgument)
+        {
+            return firstArgument / secondArgument;
+        }
+    }
+
+    public class Helper
+    {
+        public void PrintResult(Calculate action)
+        {
+            double firstArgument = 10;
+            double secondArgument = 20;
+
+            var calculationResult = action(firstArgument, secondArgument);
+
+            Console.WriteLine(calculationResult);
+        }
+    }
+
+    public class Execution
+    {
+        public void Execute()
+        {
+            var helper = new Helper();
+            var calculator = new Calculator();
+
+            helper.PrintResult(new Calculate(calculator.Add));
+            helper.PrintResult(new Calculate(calculator.Subtract));
+            helper.PrintResult(calculator.Multiply);
+            helper.PrintResult(calculator.Divide);
+        }
+    }
+}
