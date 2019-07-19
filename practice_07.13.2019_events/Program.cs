c88547b6b126c9081ae9f35e1d025e1301bcddd9
novikovkiruha <novikov.k.ru@gmail.com>
2019-07-20 00:09:55 +0300

    Task3_IDisposable was added

--- practice_07.13.2019_events/Program.cs
+++ practice_07.13.2019_events/Program.cs
@@ -1,6 +1,7 @@
 ﻿using practice_07._13._2019_events.Task1_PingPong;
 using practice_07._13._2019_events.Task1_PingPong.Task1._1_CustomDelegate;
 using practice_07._13._2019_events.Task2_HunterAndRabbit;
+using practice_07._13._2019_events.Task3_IDisposable;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,8 +15,9 @@ namespace practice_07._13._2019_events
         static void Main(string[] args)
         {
             //new GameManager(new Ping(), new Pong());
-            new GameManager(new Ping(), new Pong());
+            //new GameManager(new Ping(), new Pong());
             //new Hunting(new Hunter(), new Rabbit());
+            new FileSystem("some text").CreateFile(@"D:\temp.txt");
         }
     }
 }
---
+++ practice_07.13.2019_events/Task3_IDisposable/FileSystem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace practice_07._13._2019_events.Task3_IDisposable
+{
+    public class FileSystem : IDisposable
+    {
+        private readonly string text;
+
+        private StreamWriter writer;
+
+        public FileSystem(string text)
+        {
+            this.text = text;
+        }
+
+        public void CreateFile(string path)
+        {
+            using (this.writer = new StreamWriter(path))
+                writer.Write(this.text);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            this.writer.Dispose();
+        }
+    }
+}
