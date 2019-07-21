cf45bc8e5ff100697a03f1efc041bbfddd85f25f
novikovkiruha <novikov.k.ru@gmail.com>
2019-07-22 01:34:21 +0300

    Task1_FallingSymbols was added

---
+++ practice_07.20.2019_multithreading/Program.cs
@@ -0,0 +1,91 @@
+﻿using practice_07._20._2019_multithreading.Task1_FallingSymbols;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace practice_07._20._2019_multithreading
+{
+    class Program
+    {
+        static object locker = new object();
+
+        static int Generate(int a, int b) // Метод генерирования рандомных чисел
+        {
+            Random rand = new Random((int)DateTime.Now.Ticks & 0x0000FFFF);
+            return rand.Next(a, b);
+        }
+
+        static void Matrix(object numberColumn) // метод вывода столбцов с символами, в качестве параметра принимает позицию строки курсора
+        {
+            while (true)
+            {
+                int lenghtColumn = 0;
+                int position = 0; // Позиция столбца курсора
+                const int maxLenght = 40; // максимальная длинна столбца
+                Random rand = new Random();
+                int count = Generate(25, 39); //длинна столбца 
+                Thread.Sleep(555);
+                for (int i = 0; i != maxLenght + count; i++)
+                {
+                    lock (locker)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black; //Задаём чёрный цвет тексту
+                        for (int j = 0; j <= position; j++) // В цикле закрашиваем символы черным квадратом
+                        {
+                            Console.SetCursorPosition((int)numberColumn, j); // Устанавливаем позицию курсора
+                            Console.WriteLine("█"); // Так как мы поменяли цвет текста в консоли на чёрный, то квадрат не будет красным
+                        }
+                        if (lenghtColumn < count && position != 40)
+                        {
+                            ++lenghtColumn;
+                        }
+                        else if (position == maxLenght)
+                        {
+                            --lenghtColumn;
+                        }
+                        if (lenghtColumn >= 3)
+                        {
+                            for (int k = position - (lenghtColumn - 1); k <= position - 2; k++)
+                            {
+                                Console.SetCursorPosition((int)numberColumn, k);
+                                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                                Console.WriteLine((char)rand.Next('A', 'z'));
+                            }
+                        }
+                        if (lenghtColumn >= 2)
+                        {
+                            Console.SetCursorPosition((int)numberColumn, position - 1);
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine((char)rand.Next('A', 'z'));
+                        }
+                        if (lenghtColumn >= 1)
+                        {
+                            Console.SetCursorPosition((int)numberColumn, position);
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine((char)rand.Next('A', 'z'));
+                            if (position != maxLenght) position++;
+                        }
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            new FallingSymbols().Execute();
+            //new FallingSymbols().Matrix();
+
+            //Console.SetWindowSize(100, 42);
+            //Thread t;
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    t = new Thread(Matrix);
+            //    t.Start(i);
+            //}
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ practice_07.20.2019_multithreading/Task1_FallingSymbols/FallingSymbols.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Threading;
+
+namespace practice_07._20._2019_multithreading.Task1_FallingSymbols
+{
+    public class FallingSymbols
+    {
+        static readonly object locker = new object();
+
+        public Random random = new Random();
+
+        private readonly int consoleWidth = 100;
+
+        private readonly int consoleHeight = 50;
+
+        public FallingSymbols()
+        {
+        }
+
+        public FallingSymbols(int consoleWidth, int consoleHeight)
+        {
+            this.consoleWidth = consoleWidth;
+            this.consoleHeight = consoleHeight;
+        }
+
+        public void Execute()
+        {
+            while (true)
+            {
+                var thread = new Thread(new ThreadStart(PrintSymbols));
+                thread.Start();
+                new FallingSymbols().PrintSymbols();
+            }
+            //new FallingSymbols().PrintSymbols();
+        }
+
+        public void PrintSymbols()
+        {
+            //lock (locker)
+            //{
+            Console.SetWindowSize(this.consoleWidth, this.consoleHeight);
+            var symbolLength = this.random.Next(3, 10);
+            var leftCursorPosition = this.random.Next(0, this.consoleWidth);
+            var topCursorPosition = this.random.Next(0, this.consoleHeight);
+            //Console.SetCursorPosition(0, 0);
+
+            for (int i = 0; i < symbolLength; i++)
+            {
+                leftCursorPosition = this.random.Next(0, this.consoleWidth);
+                topCursorPosition = this.random.Next(0, this.consoleHeight);
+                //Console.SetCursorPosition(this.random.Next(0, 70), i);
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.SetCursorPosition(leftCursorPosition, topCursorPosition);
+
+                    Console.SetCursorPosition(leftCursorPosition, j);
+                    this.SetSymbolColor(i, j);
+                    var charRandom = this.random.Next(48, 126);
+                    topCursorPosition++;
+                    Console.WriteLine($"{Convert.ToChar(charRandom)}");
+
+                    Thread.Sleep(100);
+                    Console.SetCursorPosition(leftCursorPosition, this.random.Next(0, 40));
+                }
+            }
+
+            //while (true)
+            //{
+            //    leftCursorPosition = this.random.Next(0, this.consoleWidth);
+            //    topCursorPosition = this.random.Next(0, this.consoleHeight);
+            //    Console.SetCursorPosition(leftCursorPosition, topCursorPosition - symbolLength);
+            //    symbolLength = this.random.Next(3, 10);
+            //    for (int i = 0; i < symbolLength; i++)
+            //    {
+            //        //Console.SetCursorPosition(leftCursorPosition, topCursorPosition);
+            //        Console.ForegroundColor = ConsoleColor.Green;
+            //        //this.SetSymbolColor(i, j);
+            //        var charRandom = this.random.Next(48, 126);
+            //        //topCursorPosition++;
+            //        Console.WriteLine($"{Convert.ToChar(charRandom)}");
+
+            //        Thread.Sleep(400);
+            //    }
+            //}
+        }
+
+        public void SetSymbolColor(int firstValue, int secondValue)
+        {
+            if (firstValue == secondValue)
+                Console.ForegroundColor = ConsoleColor.White;
+            else if (firstValue == secondValue + 1)
+                Console.ForegroundColor = ConsoleColor.Green;
+            else
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+        }
+    }
+}
